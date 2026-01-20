bac9b918edd090c9c24c061930c50a8d4d68bf30
zhaobohao-byte <1653505623@qq.com>
2026-01-20 18:58:31 +0800

    diff阈值更小，取0.5sigma

--- source/Y1_1Pace/Y1_1Pace/tasks/manager_based/y1_1pace/pace_sim2real_env_cfg.py
+++ source/Y1_1Pace/Y1_1Pace/tasks/manager_based/y1_1pace/pace_sim2real_env_cfg.py
@@ -97,8 +97,8 @@ class TerminationsCfg:
 class CMAESOptimizerCfg:
     """CMA-ES optimizer configuration."""
     max_iteration: int = 150
-    epsilon: float = 1e-3 # 修改的
-    sigma: float = 0.25 
+    epsilon: float = 1e-5 # 修改的
+    sigma: float = 0.5 
     save_interval: int = 10
     save_optimization_process: bool = False  # consume more disk space if True, saves optimization process after finishing
 
